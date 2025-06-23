fc569cc06d3e36f7cbda79a03a4f5babdfb96beb
Abhishek Singh <abhi.r1509.git@gmail.com>
2025-06-23 14:30:34 +0530

    adding main

---
+++ ceasar_cipher.py
@@ -0,0 +1,19 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(original_text,shift_amount):
+    cipher_text = ""
+    for char in original_text:
+        shifted_position = alphabet.index(char) + shift_amount
+        shifted_position %= len(alphabet) # always between 0 - 25
+        cipher_text += alphabet[shifted_position]
+
+    print(f"Here is your encrypted text - {cipher_text}")
+
+
+encrypt(original_text=text,shift_amount=shift)
+
