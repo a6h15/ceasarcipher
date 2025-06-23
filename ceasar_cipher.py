2e4e4d025a58abff848eab0fc9b3a81e44a23d2e
Abhishek Singh <abhi.r1509.git@gmail.com>
2025-06-23 14:41:40 +0530

    de

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -4,6 +4,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+def decrypt(original_text ,shift_amount):
+    decoded_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        decoded_text += alphabet[shifted_position]
+    print(f"Here is the encoded result: {decoded_text}")
+
+decrypt(original_text=text,shift_amount=shift)
 
 def encrypt(original_text,shift_amount):
     cipher_text = ""
